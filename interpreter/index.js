b4172b8671e1a8a8a87266b8087a7c4ced91b985
Weiru Yin <weiruyin92@gmail.com>
2022-03-21 06:59:12 -0700

    Add some more operators into Smart contract language

--- interpreter/index.js
+++ interpreter/index.js
@@ -1,6 +1,16 @@
 const STOP = 'STOP';
 const ADD = 'ADD';
+const SUB = 'SUB';
+const MUL = 'MUL';
+const DIV = 'DIV';
 const PUSH = 'PUSH';
+const LT = 'LT';
+const GT = 'GT';
+const EQ = 'EQ';
+const AND = 'AND';
+const OR = 'OR';
+const JUMP = 'JUMP';
+const JUMPI = 'JUMPI';
 
 class Interpreter {
     constructor() {
@@ -9,6 +19,13 @@ class Interpreter {
             stack: [],
             code: []
         };
+    }
+
+    jump() {
+        const destination = this.state.stack.pop();
+
+        this.state.programCounter = destination;
+        this.state.programCounter--;
     }
 
     runCode(code) {
@@ -27,10 +44,40 @@ class Interpreter {
                         this.state.stack.push(value);
                         break;
                     case ADD:
+                    case SUB:
+                    case MUL:
+                    case DIV:
+                    case LT:
+                    case GT:
+                    case EQ:
+                    case AND:
+                    case OR:
                         const a = this.state.stack.pop();
                         const b = this.state.stack.pop();
 
-                        this.state.stack.push(a+b);
+                        let result;
+
+                        if (opCode === ADD) result = a + b;
+                        if (opCode === SUB) result = a - b;
+                        if (opCode === MUL) result = a * b;
+                        if (opCode === DIV) result = a / b;
+                        if (opCode === LT) result = a < b ? 1 : 0;
+                        if (opCode === GT) result = a > b ? 1 : 0;
+                        if (opCode === EQ) result = a === b ? 1 : 0;
+                        if (opCode === AND) result = a && b;
+                        if (opCode === OR) result = a || b;
+
+                        this.state.stack.push(result);
+                        break;
+                    case JUMP:
+                        this.jump();
+                        break;
+                    case JUMPI:
+                        const condition = this.state.stack.pop();
+
+                        if (condition === 1) {
+                            this.jump();
+                        }
                         break;
                     default:
                         break;
@@ -44,6 +91,46 @@ class Interpreter {
     }
 }
 
-const code = [PUSH, 2, PUSH, 3, ADD, STOP];
-const interpreter = new Interpreter();
-interpreter.runCode(code);
+let code = [PUSH, 2, PUSH, 3, ADD, STOP];
+let result = new Interpreter().runCode(code);
+console.log('Result of 3 ADD 2:', result);
+
+code = [PUSH, 2, PUSH, 3, SUB, STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of 3 SUB 2:', result);
+
+code = [PUSH, 2, PUSH, 3, MUL, STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of 3 MUL 2:', result);
+
+code = [PUSH, 2, PUSH, 3, DIV, STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of 3 DIV 2:', result);
+
+code = [PUSH, 2, PUSH, 3, LT, STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of 3 LT 2:', result);
+
+code = [PUSH, 2, PUSH, 3, GT, STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of 3 GT 2:', result);
+
+code = [PUSH, 2, PUSH, 2, EQ, STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of 2 EQ 2:', result);
+
+code = [PUSH, 1, PUSH, 0, AND, STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of 0 AND 1:', result);
+
+code = [PUSH, 1, PUSH, 0, OR, STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of 0 OR 1:', result);
+
+code = [PUSH, 6, JUMP, PUSH, 0, JUMP, PUSH, 'Jump successful', STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of JUMP:', result);
+
+code = [PUSH, 8, PUSH, 1, JUMPI, PUSH, 0, JUMP, PUSH, 'Jump successful', STOP];
+result = new Interpreter().runCode(code);
+console.log('Result of JUMPI:', result);
