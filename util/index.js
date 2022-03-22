74fe1b17b9cd47d0c5e4f9f0beed9d77c53b80ca
Weiru Yin <weiruyin92@gmail.com>
2022-03-21 19:35:38 -0700

    Add Keccak Hash
    - Install Keccak Hash: npm i js-sha3 --save

--- util/index.js
+++ util/index.js
@@ -1,7 +1,18 @@
+const keccak256 = require('js-sha3').keccak_256;
+
 const sortCharacters = data => {
     return JSON.stringify(data).split('').sort().join('');
 }
 
+const keccakHash = data => {
+    const hash = keccak256.create();
+
+    hash.update(sortCharacters(data));
+
+    return hash.hex();
+}
+
 module.exports = {
-    sortCharacters
+    sortCharacters,
+    keccakHash
 };
--- util/index.test.js
+++ util/index.test.js
@@ -1,4 +1,4 @@
-const { sortCharacters } = require('./index');
+const { sortCharacters, keccakHash } = require('./index');
 
 describe('util', () => {
     describe('sortCharacters()', () => {
@@ -10,6 +10,12 @@ describe('util', () => {
         it('creates a different string for different objects', () => {
             expect(sortCharacters({ foo: 'foo' }))
                 .not.toEqual(sortCharacters({ bar: 'bar' }));
+        });
+    });
+
+    describe('keccakHash()', () => {
+        it('produces a keccak256 hash', () => {
+            expect(keccakHash('foo')).toEqual('b2a7ad9b4a2ee6d984cc5c2ad81d0c2b2902fa410670aa3f2f4f668a1f80611c');
         });
     });
 });
