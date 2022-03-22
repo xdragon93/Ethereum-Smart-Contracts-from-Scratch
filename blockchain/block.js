7ae750f6b6c79c7f1d505af81b9e0da91529fe39
Weiru Yin <weiruyin92@gmail.com>
2022-03-21 23:07:04 -0700

    Mine block - under target hash
    Test: node ./blockchain/block.js

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,7 +1,9 @@
 const { GENESIS_DATA } = require('../config');
+const { keccakHash } = require('../util');
 
 const HASH_LENGTH = 64;
 const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
+const MAX_NONCE_VALUE = 2 ** 64;
 
 class Block {
     constructor({ blockHeaders }) {
@@ -9,7 +11,8 @@ class Block {
     }
 
     static calculateBlockTargetHash({ lastBlock }) {
-        const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(16);
+        // const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(16);
+        const value = (MAX_HASH_VALUE / 5).toString(16);
 
         if (value.length > HASH_LENGTH) {
             return 'f'.repeat(HASH_LENGTH);
@@ -19,7 +22,30 @@ class Block {
     }
 
     static mineBlock({ lastBlock, beneficiary }) {
+        const target = this.calculateBlockTargetHash({ lastBlock });
+        let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash;
 
+        do {
+            timestamp = Date.now();
+            truncatedBlockHeaders = {
+                parentHash: keccakHash(lastBlock.blockHeaders),
+                beneficiary,
+                difficulty: lastBlock.blockHeaders.difficulty + 1,
+                number: lastBlock.blockHeaders.number + 1,
+                timestamp
+            };
+            header = keccakHash(truncatedBlockHeaders);
+            nonce = Math.floor(Math.random() * MAX_NONCE_VALUE);
+
+            underTargetHash = keccakHash(header + nonce);
+        } while (underTargetHash > target);
+
+        console.log('underTargetHash', underTargetHash);
+        console.log('target', target);
+
+        return new this({
+            blockHeaders: { ...truncatedBlockHeaders, nonce }
+        });
     }
 
     static genesis() {
@@ -28,3 +54,10 @@ class Block {
 }
 
 module.exports = Block;
+
+const block = Block.mineBlock({
+    lastBlock: Block.genesis(),
+    beneficiary: 'foo'
+});
+
+console.log('block', block);
